e4e81506c32ce705c99c4d8a4a8c46e9e13d4b38
jstone18 <jason.stone0808@gmail.com>
2020-01-22 14:29:43 -0500

    Updates

---
+++ precache-manifest.b73928cbf10d33e31484287fb27aa493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4ae36b7e7bcd165e13896c9c9abbc4",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "d600024b8f9ae1c3b61e",
+    "url": "/monsters-rolodex-app/static/css/main.39f62575.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex-app/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-app/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d600024b8f9ae1c3b61e",
+    "url": "/monsters-rolodex-app/static/js/main.ebf11747.chunk.js"
+  },
+  {
+    "revision": "32a7496782ec530cbdef",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.a793a3a3.js"
+  }
+]);

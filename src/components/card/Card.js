ef634eadf42d9b9b874f1a93f83c8f0069b0f8df
jstone18 <jason.stone0808@gmail.com>
2020-01-22 11:18:22 -0500

    Add monster images from robohash. Update styling.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,7 +4,12 @@ import "./card.css";
 const Card = ({ monster }) => {
 	return (
 		<div className="card-container">
+			<img
+				src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+				alt="monster"
+			/>
 			<h1>{monster.name}</h1>
+			<p>{monster.email}</p>
 		</div>
 	);
 };

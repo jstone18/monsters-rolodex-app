6b4a4227d6742245d906ad8ac6c05f5822209a15
jstone18 <jason.stone0808@gmail.com>
2020-01-22 09:48:30 -0500

    Fetch users from api and display then in new card-list component. Add grid styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
+import CardList from "./components/card-list/CardList";
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(res => res.json())
+			.then(users => this.setState({ monsters: users }));
+	}
+
 	render() {
+		const monsters = this.state.monsters.map(monster => (
+			<h1 key={monster.id}>{monster.name}</h1>
+		));
 		return (
 			<div className="App">
-				<h1>Hello</h1>
+				<CardList>{monsters}</CardList>
 			</div>
 		);
 	}
